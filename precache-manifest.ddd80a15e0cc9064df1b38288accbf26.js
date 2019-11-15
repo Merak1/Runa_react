7470a5bfbec2e60da697849f4e4278150c2d61a9
Merak1 <eddie_comes_for_you@hotmail.es>
2019-11-15 16:49:43 -0600

    Updates

---
+++ precache-manifest.ddd80a15e0cc9064df1b38288accbf26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c8f0c6408616d0293d7efd3076e389",
+    "url": "/Runa_react/index.html"
+  },
+  {
+    "revision": "c76da31b4ab0b5663322",
+    "url": "/Runa_react/static/css/main.7965ddac.chunk.css"
+  },
+  {
+    "revision": "6ff3a6a056e81c299dac",
+    "url": "/Runa_react/static/js/2.447f8621.chunk.js"
+  },
+  {
+    "revision": "c76da31b4ab0b5663322",
+    "url": "/Runa_react/static/js/main.a30a8017.chunk.js"
+  },
+  {
+    "revision": "3b4ac0f5f42bcb89dc95",
+    "url": "/Runa_react/static/js/runtime-main.1937fa71.js"
+  }
+]);
